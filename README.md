3f28154b5f35c8850ac6feeea233b6975cdefea1
webber <iewebber@126.com>
2017-06-16 18:09:05 +0800

    添加文档

--- README.md
+++ README.md
@@ -200,5 +200,5 @@ http://wiki.mob.com/Android_%E5%BF%AB%E9%80%9F%E9%9B%86%E6%88%90%E6%8C%87%E5%8D%
 
 注意：使用时请参照需要去掉一些无用lib,减少包大小
 
-## sdk-share
+## sdk-net
 网络框架，文档待写
--- app/src/main/java/com/webber/mcorelibspace/MainActivity.java
+++ app/src/main/java/com/webber/mcorelibspace/MainActivity.java
@@ -1,7 +1,11 @@
 package com.webber.mcorelibspace;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,6 +21,8 @@ import com.webber.mcorelibspace.demo.map.MapActivity;
 import com.webber.mcorelibspace.demo.net.NetDemoActivity;
 import com.webber.mcorelibspace.demo.pay.PayDemoActivity;
 import com.webber.mcorelibspace.demo.share.ShareActivity;
+
+import java.util.ArrayList;
 
 import cn.sharesdk.SocialShareHelper;
 
@@ -51,6 +57,57 @@ public class MainActivity extends Activity {
         });
         mAdapter.setData(DEMOS);
         mDemoRv.setAdapter(mAdapter);
+        getPermission();
+    }
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private String permissionInfo;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void getPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /**
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+			/*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(permission)){
+                return true;
+            }else{
+                permissionsList.add(permission);
+                return false;
+            }
+
+        }else{
+            return true;
+        }
     }
 
     public class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.ViewHolder> {
