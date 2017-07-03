fa23968d304ed9ff8e34eec1377136e333bfb965
webber <iewebber@126.com>
2017-07-03 18:16:01 +0800

    添加意图跳转

--- sdk-core/src/main/java/com/android_mobile/core/utiles/IntentUtils.java
+++ sdk-core/src/main/java/com/android_mobile/core/utiles/IntentUtils.java
@@ -37,47 +37,6 @@ public class IntentUtils {
         context.startActivity(intent);
     }
 
-    /**
-     * 打开浏览器
-     */
-    public static void openBrowser(Context context, String url) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
-        context.startActivity(intent);
-    }
-
-    /**
-     * 打开图片
-     */
-    public static void openImage(Context context, String path) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        // intent.setDataAndType(Uri.parse("file:///mnt/sdcard/file/1.jgp"),
-        // "image/*");
-        intent.setDataAndType(Uri.parse("file:///" + path), "image/*");
-        context.startActivity(intent);
-    }
-
-    /**
-     * 打开音频
-     */
-    public static void openAudio(Context context, String path) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.parse("file:///" + path), "audio/*");
-        context.startActivity(intent);
-    }
-
-    /**
-     * 打开视频文件
-     */
-    public static void openVideo(Context context, String path) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.parse("file:///" + path), "video/*");
-        context.startActivity(intent);
-    }
 
     /**
      * 打开系统摄像头录像,并保存为图片
@@ -202,5 +161,127 @@ public class IntentUtils {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         intent.putExtra("output", uri);
         context.startActivityForResult(intent, REQUEST_CODE_CAMERA);
+    }
+
+    //android获取一个用于打开HTML文件的intent
+    public static Intent getHtmlFileIntent(String Path) {
+        File file = new File(Path);
+        Uri uri = Uri.parse(file.toString()).buildUpon().encodedAuthority("com.android.htmlfileprovider").scheme("content").encodedPath(file.toString()).build();
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.setDataAndType(uri, "text/html");
+        return intent;
+    }
+
+    //android获取一个用于打开图片文件的intent
+    public static Intent getImageFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "image/*");
+        return intent;
+    }
+
+    //android获取一个用于打开PDF文件的intent
+    public static Intent getPdfFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/pdf");
+        return intent;
+    }
+
+    //android获取一个用于打开文本文件的intent
+    public static Intent getTextFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "text/plain");
+        return intent;
+    }
+
+    //android获取一个用于打开音频文件的intent
+    public static Intent getAudioFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("oneshot", 0);
+        intent.putExtra("configchange", 0);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "audio/*");
+        return intent;
+    }
+
+    //android获取一个用于打开视频文件的intent
+    public static Intent getVideoFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("oneshot", 0);
+        intent.putExtra("configchange", 0);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "video/*");
+        return intent;
+    }
+
+
+    //android获取一个用于打开CHM文件的intent
+    public static Intent getChmFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/x-chm");
+        return intent;
+    }
+
+
+    //android获取一个用于打开Word文件的intent
+    public static Intent getWordFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/msword");
+        return intent;
+    }
+
+    //android获取一个用于打开Excel文件的intent
+    public static Intent getExcelFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/vnd.ms-excel");
+        return intent;
+    }
+
+    //android获取一个用于打开PPT文件的intent
+    public static Intent getPPTFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        return intent;
+    }
+
+    //android获取一个用于打开apk文件的intent
+    public static Intent getApkFileIntent(String Path) {
+        File file = new File(Path);
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        return intent;
     }
 }
