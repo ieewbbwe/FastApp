95b6441e47ac5b9da0ae56376493bae6d19865cf
webber <iewebber@126.com>
2017-08-07 18:16:58 +0800

    MVP示例

---
+++ app/src/main/java/com/webber/mcorelibspace/demo/mvp/MVPActivity.java
@@ -0,0 +1,15 @@
+package com.webber.mcorelibspace.demo.mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webber.mcorelibspace.R;
+
+public class MVPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvp);
+    }
+}
---
+++ app/src/main/java/com/webber/mcorelibspace/demo/mvp/MVPPresenter.java
@@ -0,0 +1,9 @@
+package com.webber.mcorelibspace.demo.mvp;
+
+/**
+ * Created by mxh on 2017/8/7.
+ * Describe：
+ */
+
+public class MVPPresenter {
+}
