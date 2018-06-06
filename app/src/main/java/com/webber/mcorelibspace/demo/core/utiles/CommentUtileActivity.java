5e3164fe164c71dc87fc16fdb9b733f57e1717b3
picher <ieewbber@gmail.com>
2018-06-06 18:04:22 +0800

    sync

---
+++ app/src/main/java/com/webber/mcorelibspace/demo/core/utiles/CommentUtileActivity.java
@@ -0,0 +1,22 @@
+package com.webber.mcorelibspace.demo.core.utiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webber.mcorelibspace.R;
+
+
+/**
+ * 常用的工具包Demo
+ */
+public class CommentUtileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_utile);
+        // 获取设备信息
+        // 弹框Dialog
+        //
+    }
+}
